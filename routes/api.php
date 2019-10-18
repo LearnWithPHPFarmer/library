565251aef154d7e4f0548f6d3aa7cd199023cf86
Juyal Ahmed <tojibon@gmail.com>
2019-10-18 23:24:13 +0200

    01 - Basic Authors CRUD added

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+Route::get('authors', 'AuthorsController@index');
+Route::get('authors/{id}', 'AuthorsController@show');
+Route::post('authors', 'AuthorsController@store');
+Route::put('authors/{id}', 'AuthorsController@update');
+Route::delete('authors/{id}', 'AuthorsController@destroy');
